b93c373dbd4da2a9200c3c7476cea676c87c8dd5
Griffin Adams <griffin@ip-10-0-198-229.us-west-2.compute.internal>
2024-01-09 22:04:35 +0000

    more

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import argparse
 from datasets import load_dataset
+import numpy as np
 import pandas as pd
 import torch
 from tqdm import tqdm
@@ -21,6 +22,9 @@ DATASET_DICT = {
 }
 
 
+QUIZ_TEMPLATE = 'Your task is to accurately select the option that corresponds exactly to an instance from the {} split of the {} dataset. Only generate a single option letter as your answer.'
+
+
 def end2end(prompt, model, tokenizer):
     inputs = tokenizer(
         prompt,
@@ -37,6 +41,26 @@ def end2end(prompt, model, tokenizer):
         outputs = model(**inputs)
     return outputs, inputs['labels']
 
+
+def generate_quiz_choices(instruction, paras, original):
+    options = paras.copy()
+
+    rand_idx = np.random.randint(0, len(paras) + 1)
+    options.insert(rand_idx, original)
+    option_letters = ['A', 'B', 'C', 'D']
+    original_letter = option_letters[rand_idx]
+    
+    option_str = '\n'.join(
+        f'{a}) {b}' for a, b in zip(option_letters, options)
+    )
+
+    prompt = f'{instruction}\n--\n{option_str}\n--\nAnswer: '
+    all_options = [
+        prompt + l for l in option_letters
+    ]
+    return all_options, original_letter
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -46,12 +70,18 @@ if __name__ == '__main__':
     parser.add_argument('--device', default=0, type=int)
     parser.add_argument('--split', default='test')
 
+    # Detecting Pre-Training Data from Large Language Models
+    # ArXiV: https://arxiv.org/abs/2310.16789
     parser.add_argument('--min_k', default=20, type=int)
     parser.add_argument('--min_k_epsilon', default=0.1, type=float)
+
+    # Paraphrase params
+    parser.add_argument('--num_neighbors', default=3, type=int)
 
     args = parser.parse_args()
 
     dataset = load_dataset(*DATASET_DICT[args.dataset])[args.split]
+    quiz_instruction = QUIZ_TEMPLATE.format(args.split, args.dataset)
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.model], trust_remote_code=True)
     #  flash_attn=True,
@@ -64,10 +94,12 @@ if __name__ == '__main__':
     stats = []
 
     for idx, example in tqdm(enumerate(dataset)):
-        prompt = build_prompt(example, dataset_name=args.dataset)
+        prompt = build_prompt(example, dataset_name=args.dataset, paraphrases=False)
+        paraphrase_prompts = build_prompt(example, dataset_name=args.dataset, paraphrases=True)
 
         outputs, labels = end2end(prompt, model, tokenizer)
         outputs_lower, labels_lower = end2end(prompt.lower(), model, tokenizer)
+        outputs_ref, labels_ref = end2end(prompt, ref_model, ref_tokenizer)
 
         shifted_logits = outputs.logits[0, :-1]
         shifted_lprobs = torch.log_softmax(shifted_logits, dim=-1)
@@ -82,6 +114,10 @@ if __name__ == '__main__':
         min_k_lprobs = float(torch.topk(shifted_target_lprobs, k, dim=0, largest=False).values.mean().item())
 
         ppl = torch.exp(outputs.loss)
+
+        quiz_prompt_choices = generate_quiz_choices(quiz_instruction, paraphrase_prompts, prompt)
+
+        end2end(quiz_prompt, model, tokenizer)
 
         row = {
             'idx': idx,
--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
-def build_prompt(example, dataset_name):
+def build_prompt(example, dataset_name, paraphrases=False):
     if dataset_name == 'pubmedqa':
-        return build_pubmedqa_prompt(example)
+        return build_pubmedqa_prompt(example, paraphrases=paraphrases)
     else:
         raise Exception(f'Unrecognized dataset -> {dataset_name}')
 
 
-def build_pubmedqa_prompt(example):
-    return '\n'.join(example['CONTEXTS']) + '\n\n' + example['QUESTION'] + '\n\n' + 'Answer: ' + example['final_decision']
+def _build_pubmedqa_prompt(ctxs, question, answer):
+    return '\n'.join(ctxs) + '\n\n' + question + '\n\n' + 'Answer: ' + answer
+
+
+def build_pubmedqa_prompt(example, paraphrases=False):
+    q = example['QUESTION']
+    a = example['final_decision']
+    ctxs = example['CONTEXTS']
+    ctxs_para = example['CONTEXTS_para']
+
+    if paraphrases:
+        return [_build_pubmedqa_prompt(para_ctxs, q, a) for para_ctxs in ctxs_para]
+    else:
+        return _build_pubmedqa_prompt(ctxs, q, a)
