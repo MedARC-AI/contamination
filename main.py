30037a85a74e6b44afd09e75850e490ced502665
Griffin Adams <griffin@ip-10-0-198-229.us-west-2.compute.internal>
2024-01-09 20:57:01 +0000

    more

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 from datasets import load_dataset
 import pandas as pd
+import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
@@ -26,6 +27,9 @@ if __name__ == '__main__':
     parser.add_argument('--device', default=0, type=int)
     parser.add_argument('--split', default='test')
 
+    parser.add_argument('--min_k', default=20, type=int)
+    parser.add_argument('--min_k_epsilon', default=0.1, type=float)
+
     args = parser.parse_args()
 
     dataset = load_dataset(*DATASET_DICT[args.dataset])[args.split]
@@ -35,18 +39,36 @@ if __name__ == '__main__':
 
     stats = []
 
-    for example in tqdm(dataset):
+    for idx, example in tqdm(enumerate(dataset)):
         prompt = build_prompt(example, dataset_name=args.dataset)
 
         inputs = tokenizer(
             prompt,
             max_length=4096,
             truncation=True,
+            # return_attention_mask=False,
+            return_tensors='pt'
         )
 
         inputs = {k: v.to(model.device) for k, v in inputs.items()}
 
-        outputs = model(**inputs)
+        labels = inputs['input_ids'][0, 1:]
 
-        print('here')
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        logits = outputs.logits[0, :-1]
+        lprobs = torch.log_softmax(logits, dim=-1)
+
+        target_lprobs = torch.gather(lprobs, 1, labels.view(-1, 1)).squeeze(-1)
+
+        seq_len = len(target_lprobs)
+
+        k = min(args.min_k, seq_len)
+        min_k_lprobs = float(torch.topk(target_lprobs, k, dim=0, largest=False).values.mean().item())
+
+        row = {
+            'idx': idx,
+            'min_k':  min_k_lprobs,
+        }
 
