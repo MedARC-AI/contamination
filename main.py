8957512b5ef7d5308f6ace8ba1a9df1a6b870aae
Griffin Adams <griffin@ip-10-0-198-229.us-west-2.compute.internal>
2024-01-09 21:13:06 +0000

    more

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import argparse
 from datasets import load_dataset
 import pandas as pd
@@ -19,10 +21,27 @@ DATASET_DICT = {
 }
 
 
+def end2end(prompt, model, tokenizer):
+    inputs = tokenizer(
+        prompt,
+        max_length=4096,
+        truncation=True,
+        # return_attention_mask=False,
+        return_tensors='pt'
+    )
+
+    inputs = {k: v.to(model.device) for k, v in inputs.items()}
+    inputs['labels'] = inputs['input_ids']  # Will be shifted over inside AutoModelForCausalLM subclass
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+    return outputs, inputs['labels']
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--model', default='phi2', choices=list(MODEL_DICT.keys()))
+    parser.add_argument('--ref_model', default='phi2', choices=list(MODEL_DICT.keys()))
     parser.add_argument('--dataset', default='pubmedqa', choices=list(DATASET_DICT.keys()))
     parser.add_argument('--device', default=0, type=int)
     parser.add_argument('--split', default='test')
@@ -35,40 +54,47 @@ if __name__ == '__main__':
     dataset = load_dataset(*DATASET_DICT[args.dataset])[args.split]
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.model], trust_remote_code=True)
+    #  flash_attn=True,
     model = AutoModelForCausalLM.from_pretrained(MODEL_DICT[args.model], trust_remote_code=True).eval().to(args.device)
+
+    ref_tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.ref_model], trust_remote_code=True)
+    #  flash_attn=True,
+    ref_model = AutoModelForCausalLM.from_pretrained(MODEL_DICT[args.ref_model], trust_remote_code=True).eval().to(args.device)
 
     stats = []
 
     for idx, example in tqdm(enumerate(dataset)):
         prompt = build_prompt(example, dataset_name=args.dataset)
 
-        inputs = tokenizer(
-            prompt,
-            max_length=4096,
-            truncation=True,
-            # return_attention_mask=False,
-            return_tensors='pt'
-        )
+        outputs, labels = end2end(prompt, model, tokenizer)
+        outputs_lower, labels_lower = end2end(prompt.lower(), model, tokenizer)
 
-        inputs = {k: v.to(model.device) for k, v in inputs.items()}
+        shifted_logits = outputs.logits[0, :-1]
+        shifted_lprobs = torch.log_softmax(shifted_logits, dim=-1)
 
-        labels = inputs['input_ids'][0, 1:]
+        shifted_labels = labels[0, 1:]
 
-        with torch.no_grad():
-            outputs = model(**inputs)
+        shifted_target_lprobs = torch.gather(shifted_lprobs, 1, shifted_labels.view(-1, 1)).squeeze(-1)
 
-        logits = outputs.logits[0, :-1]
-        lprobs = torch.log_softmax(logits, dim=-1)
-
-        target_lprobs = torch.gather(lprobs, 1, labels.view(-1, 1)).squeeze(-1)
-
-        seq_len = len(target_lprobs)
+        seq_len = len(shifted_target_lprobs)
 
         k = min(args.min_k, seq_len)
-        min_k_lprobs = float(torch.topk(target_lprobs, k, dim=0, largest=False).values.mean().item())
+        min_k_lprobs = float(torch.topk(shifted_target_lprobs, k, dim=0, largest=False).values.mean().item())
+
+        ppl = torch.exp(outputs.loss)
 
         row = {
             'idx': idx,
             'min_k':  min_k_lprobs,
+            'ppl': ppl,
         }
 
+        stats.append(row)
+    
+    stats = pd.DataFrame(stats)
+    out_fn = os.path.join('results', f'{args.model}_{args.dataset}.csv')
+    print(f'Saving to {out_fn}...')
+    stats.to_csv(out_fn, index=False)
+
+    print('Printing out average scores...')
+    print(stats.select_dtypes('number').mean())
