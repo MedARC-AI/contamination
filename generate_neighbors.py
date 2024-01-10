b00a81fd581244107e3e1aace21cff3f6d349dc4
Griffin Adams <griffin@ip-172-17-22-141.us-west-2.compute.internal>
2024-01-10 21:31:06 +0000

    More

--- generate_neighbors.py
+++ generate_neighbors.py
@@ -1,7 +1,6 @@
 import os
 
 import argparse
-import backoff
 from datasets import load_dataset
 import numpy as np
 from openai import OpenAI
@@ -11,35 +10,37 @@ from utils import build_prompt
 from main import DATASET_DICT
 
 
-assert 'OPENAI_KEY' in os.environ
+assert 'OPENAI_API_KEY' in os.environ
 client = OpenAI(
     # This is the default and can be omitted
-    api_key=os.environ.get('OPENAI_KEY'),
+    api_key=os.environ.get('OPENAI_API_KEY'),
+    base_url='https://east-us-2-llm.openai.azure.com/',
+    # api_version='2023-05-15',
+    # api_type='azure',
 )
 
 
 # Figure 4 from https://arxiv.org/pdf/2311.06233.pdf
-PARAPHRASE_INSTRUCTION = """
-Instruction: Your task is to create a three-choice quiz by only replacing the words in the provided text with their synonyms.
-The meaning and sentence structure of the three new options must exactly mirror every detail in the text. You must not
-include the provided text as an option. You must make sure that:
+THREE_PARAPHRASE_INSTRUCTION = ("""
+Instruction: Your task is to create a three-choice quiz by only replacing the words in the provided text with their synonyms. The meaning and sentence structure of the three new options must exactly mirror every detail in the text. You must not include the provided text as an option. You must make sure that:
 (1) You generate three distinct options based on the provided text;
 (2) Options are ordered;
 (3) There is not any extra explanation; and
 (4) You comply with every specific symbol and letter detail in the given text.
-"""
+""").strip()
 
 
 def chatgpt(messages, model='gpt-4', temperature=0.1, max_tokens=2048):
     completion = client.with_options(max_retries=5).chat.completions.create(
-        model=model, messages=messages, temperature=temperature, max_tokens=max_tokens
+        model=model, messages=messages, temperature=temperature, max_tokens=max_tokens,
+        deployment='eval-4'
     )
     return completion.choices[0].message.content
 
 
 def gen_prompt_and_neighbor_prompts(example, dataset_name='pubmedqa'):
     original_prompt = build_prompt(example, dataset_name=dataset_name, paraphrases=False)
-    para_prompt = f'{PARAPHRASE_INSTRUCTION}\n--\n{original_prompt}\n--\n'
+    para_prompt = f'{THREE_PARAPHRASE_INSTRUCTION}\n--\n{original_prompt}\n--\n'
 
     messages = [
         {'role': 'system', 'content': 'You are a helpful assistant for text paraphrasing.'},
@@ -49,7 +50,13 @@ def gen_prompt_and_neighbor_prompts(example, dataset_name='pubmedqa'):
     gpt_output = chatgpt(messages)
 
     # Parse output
-    print('here')
+    parsed_paraphrases = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), gpt_output.split('--'))))
+    assert len(parsed_paraphrases) == 3
+
+    return {
+        'original': original_prompt,
+        'paraphrases': parsed_paraphrases
+    }
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 import os
+import zlib
 
 import argparse
 from datasets import load_dataset
@@ -9,6 +11,11 @@ from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from utils import *
+
+
+INPUT_TYPE_DICT = {
+    'pubmedqa': 'a set of relevant PubMed abstracts'
+}
 
 
 MODEL_DICT = {
@@ -23,6 +30,45 @@ DATASET_DICT = {
 
 
 QUIZ_TEMPLATE = 'Your task is to accurately select the option that corresponds exactly to an instance from the {} split of the {} dataset. Only generate a single option letter as your answer.'
+GUIDED_TEMPLATE = 'Instruction: You are provided with {} from the {} split of the {} dataset. Given a target answer, generate the question as it appears in the dataset.'
+GENERAL_TEMPLATE = 'Instruction: Generate a question given {} and a given a target answer.'
+
+
+def calculate_entropy(data):
+    # Calculate the probability of each byte in the data
+    byte_count = len(data)
+    byte_freq = [data.count(byte) / byte_count for byte in set(data)]
+
+    # Calculate entropy using Shannon's entropy formula
+    entropy = -sum(p * math.log2(p) for p in byte_freq)
+    return entropy
+
+
+def calculate_zlib_entropy(text):
+    # Compress the text using zlib
+    compressed_data = zlib.compress(text.encode())
+
+    # Calculate the entropy of the compressed data
+    entropy = calculate_entropy(compressed_data)
+    return entropy
+
+
+def generate(prompts, model, tokenizer):
+    inputs = tokenizer(
+        prompts,
+        max_length=4096,
+        padding='longest',
+        truncation=True,
+        return_attention_mask=True,
+        return_tensors='pt'
+    )
+
+    inputs = {k: v.to(model.device) for k, v in inputs.items()}
+
+    with torch.no_grad():
+        outputs = model.generate(**inputs)
+    # TODO decode
+    return outputs.sequences
 
 
 def end2end(prompt, model, tokenizer):
@@ -58,11 +104,18 @@ def construct_quiz(instruction, paras, original):
     return prompt, option_letters, original_letter
 
 
+def construct_qg_prompt(instruction, example):
+    # Specific to each dataset
+    inputs = '\n'.join(example['CONTEXTS'])
+    answer = example['final_decision']
+    return f'{instruction}\n\n{inputs}\n\nAnswer: {answer}\n\nQuestion: '
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--model', default='phi2', choices=list(MODEL_DICT.keys()))
-    parser.add_argument('--ref_model', default='phi2', choices=list(MODEL_DICT.keys()))
+    parser.add_argument('--small_model', default='phi2', choices=list(MODEL_DICT.keys()))
     parser.add_argument('--dataset', default='pubmedqa', choices=list(DATASET_DICT.keys()))
     parser.add_argument('--device', default=0, type=int)
     parser.add_argument('--split', default='test')
@@ -79,14 +132,16 @@ if __name__ == '__main__':
 
     dataset = load_dataset(*DATASET_DICT[args.dataset])[args.split]
     quiz_instruction = QUIZ_TEMPLATE.format(args.split, args.dataset)
+    guided_instruction = GUIDED_TEMPLATE.format(INPUT_TYPE_DICT[args.dataset], args.split, args.dataset)
+    general_instruction = GENERAL_TEMPLATE.format(INPUT_TYPE_DICT[args.dataset])
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.model], trust_remote_code=True)
     #  flash_attn=True,
     model = AutoModelForCausalLM.from_pretrained(MODEL_DICT[args.model], torch_dtype='auto', trust_remote_code=True).eval().to(args.device)
 
-    ref_tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.ref_model], trust_remote_code=True)
+    small_tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.small_model], trust_remote_code=True)
     #  flash_attn=True,
-    ref_model = AutoModelForCausalLM.from_pretrained(MODEL_DICT[args.ref_model], torch_dtype='auto', trust_remote_code=True).eval().to(args.device)
+    small_model = AutoModelForCausalLM.from_pretrained(MODEL_DICT[args.small_model], torch_dtype='auto', trust_remote_code=True).eval().to(args.device)
 
     stats = []
 
@@ -98,36 +153,67 @@ if __name__ == '__main__':
 
         outputs, labels = end2end(prompt, model, tokenizer)
         outputs_lower, labels_lower = end2end(prompt.lower(), model, tokenizer)
-        outputs_ref, labels_ref = end2end(prompt, ref_model, ref_tokenizer)
+        outputs_small, labels_small = end2end(prompt, small_model, small_tokenizer)
 
+        para_outputs = [
+            end2end(pp, model, tokenizer) for pp in paraphrase_prompts
+        ]
+
+        # PAPER: Time Travel in LLMs: Tracing Data Contamination in Large Language Models
+        # LINK: https://arxiv.org/abs/2308.08493
+        guided_prompt = construct_qg_prompt(guided_instruction)
+        general_instruction = construct_qg_prompt(general_instruction)
+
+        # Truncate last token logit
         shifted_logits = outputs.logits[0, :-1]
+        # Shift labels 1 to right
         shifted_lprobs = torch.log_softmax(shifted_logits, dim=-1)
-
         shifted_labels = labels[0, 1:]
-
+        # Compute token-level logprobs
         shifted_target_lprobs = torch.gather(shifted_lprobs, 1, shifted_labels.view(-1, 1)).squeeze(-1)
-
         seq_len = len(shifted_target_lprobs)
 
+        # Paper: Detecting Pretraining Data from Large Language Models
+        # LINK: https://arxiv.org/abs/2310.16789
         k = min(args.min_k, seq_len)
         min_k_lprobs = float(torch.topk(shifted_target_lprobs, k, dim=0, largest=False).values.mean().item())
 
-        ppl = torch.exp(outputs.loss)
-
+        # PAPER: Data Contamination Quiz: A Tool to Detect and Estimate Contamination in Large Language Models
+        # LINK: https://arxiv.org/pdf/2311.06233.pdf
         quiz_prompt, option_letters, original_letter = construct_quiz(quiz_instruction, paraphrase_prompts[:min(n_para, 3)], prompt)
-
         quiz_output, _ = end2end(quiz_prompt, model, tokenizer)
         option_ids = tokenizer.convert_tokens_to_ids([' ' + l for l in option_letters])
         quiz_final_logit = quiz_output.logits[0, -1, :]
         quiz_pred_letter = option_letters[int(torch.argmax(quiz_final_logit[option_ids]))]
-
         quiz_score = 1 if quiz_pred_letter == original_letter else 0
+
+        # PAPER: Membership Inference Attacks against Language Models via Neighbourhood Comparison
+        # LINK: https://aclanthology.org/2023.findings-acl.719.pdf
+        avg_para_loss = float(np.mean([po.loss for po in para_outputs]))
+        # CrossEntropyLoss(original) - Mean(CrossEntropyLoss(p) for p in paraphrased)
+        neighbor_loss_delta = outputs.loss - avg_para_loss
+
+        # PAPER: Extracting Training Data from Large Language Models
+        # LINK: https://arxiv.org/abs/2012.07805
+        orig_ppl = torch.exp(outputs.loss)
+        lower_ppl = torch.exp(outputs_lower.loss)
+        small_ppl = torch.exp(outputs_small.loss)
+
+        lower_ratio = orig_ppl / lower_ppl
+        small_lratio = float(np.log(orig_ppl) / np.log(small_ppl))
+
+        zlib_entropy = calculate_zlib_entropy(prompt)
+        zlib_lratio = float(np.log(orig_ppl) / zlib_entropy)
 
         row = {
             'idx': idx,
             'min_k':  min_k_lprobs,
-            'ppl': ppl,
             'quiz_score': quiz_score,
+            'neighbor_loss_delta': neighbor_loss_delta,
+            'ppl': orig_ppl,
+            'lower_ratio': lower_ratio,
+            'small_lratio': small_lratio,
+            'zlib_lratio': zlib_lratio
         }
 
         stats.append(row)
